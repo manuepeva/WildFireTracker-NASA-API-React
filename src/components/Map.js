4b174c5e8daca9bb15603388160149e1e0f1d833
ManuelP <manuepeva@gmail.com>
2020-11-29 11:05:02 -0500

    deploying project to netlify

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,17 +1,31 @@
+import { useState } from 'react'
 import GoogleMapReact from 'google-map-react'
+import LocationMarker from './LocationMarker'
+import LocationInfoBox from './LocationInfoBox'
 
-const Map = ({center, zoom}) => {
+// define constants
+const NATURAL_EVENT_WILDFIRE = 8;
+
+const Map = ({ eventData, center, zoom }) => {
+    const [locationInfo, setLocationInfo] = useState(null)
+
+    const markers = eventData.map((ev, index) => {
+        if(ev.categories[0].id === NATURAL_EVENT_WILDFIRE) {
+            return <LocationMarker key={index} lat={ev.geometries[0].coordinates[1]} lng={ev.geometries[0].coordinates[0]} onClick={() => setLocationInfo({ id: ev.id, title: ev.title })} />
+        }
+        return null
+    })
+
     return (
         <div className="map">
-            <h1>test here</h1>
             <GoogleMapReact
-                bootstrapURLKeys={{
-                    key: 'AIzaSyDG5Q3wIguxWGPQg1D0lr4x8VBBzea1ytQ'
-                }}
-                defaultCenter={center}
-                defaultZoom={zoom}
+                bootstrapURLKeys={{ key: 'AIzaSyDG5Q3wIguxWGPQg1D0lr4x8VBBzea1ytQ' }}
+                defaultCenter={ center }
+                defaultZoom={ zoom }
             >
+                {markers}
             </GoogleMapReact>
+            {locationInfo && <LocationInfoBox info={locationInfo} />}
         </div>
     )
 }
@@ -19,9 +33,9 @@ const Map = ({center, zoom}) => {
 Map.defaultProps = {
     center: {
         lat: 42.3265,
-        lon: -122.8756
+        lng: -122.8756
     },
-    zoom: 2
+    zoom: 6
 }
 
 export default Map
